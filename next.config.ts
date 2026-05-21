1f13a9d41a1216daaa75b000b6726a9270be373b
IJnkn0wn5 <olegmr630@gmail.com>
2026-05-21 12:39:15 +0300

    fix_for_host

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+ webpack(config) {
+    // Говорим Next.js, как правильно импортировать звуки (включая ogg из kokopu-react)
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|m4a)$/i,
+      type: 'asset/resource',
+    });
+
+    return config;
+  },
+  
 };
 
 export default nextConfig;
