9367bea5fdbcd5f2a2fa0eafdbb248ba910cea1d
IJnkn0wn5 <olegmr630@gmail.com>
2026-06-21 22:54:36 +0300

    fix3

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  
-  turbopack: {
-    rules: {
-      '*.ogg': {
-        loaders: [], // Пустой массив лоадеров решает ошибку 'is not iterable'
-        as: 'url',   // Говорит Turbopack импортировать файл как текстовую ссылку-путь
-      },
-    },
-  },
 };
 
 export default nextConfig;
