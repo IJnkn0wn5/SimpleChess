9588d19ae43e2a8041e7b22eed52af27571953f5
IJnkn0wn5 <olegmr630@gmail.com>
2026-06-21 22:24:26 +0300

    ogg_fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  turbopack: {
+    rules: {
+      '*.ogg': {
+        loaders: ['file-loader'],
+        as: 'resource',
+      },
+    },
+  },
 };
 
 export default nextConfig;
