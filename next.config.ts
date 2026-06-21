e6346756642ae4840dc97919916da45f54fbebc5
IJnkn0wn5 <olegmr630@gmail.com>
2026-06-21 23:26:06 +0300

    fix6

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   
-  webpack(config) {
-    // Говорим Webpack обрабатывать аудиофайлы как статические ресурсы
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/chunks/[name].[hash][ext]',
-      },
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
